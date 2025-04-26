4cc808b91d47b99563f649e9f98383c1d456b540
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-04-26 16:02:35 +0200

    Updated getMatchingCatsArray() and added getSingleMatchingCat()

--- index.js
+++ index.js
@@ -5,19 +5,41 @@ const getImageBtn = document.getElementById("get-image-btn");
 const gifCheckbox = document.getElementById("gif-checkbox");
 
 controlContainer.addEventListener("change", highlightRadio);
-getImageBtn.addEventListener("click", getMatchingCatsArray);
+getImageBtn.addEventListener("click", renderImage);
+
+function renderImage() {
+  getSingleMatchingCat();
+}
 
 function getMatchingCatsArray() {
-  const isChecked = document.querySelector('input[type="radio"]:checked');
-  const isGif = gifCheckbox.checked;
-
-  if (isChecked) {
+  if (document.querySelector('input[type="radio"]:checked')) {
     const selectedEmotion = document.querySelector(
       'input[type="radio"]:checked'
     ).value;
-  }
+    const isGifChecked = gifCheckbox.checked;
 
-  console.log(isGif);
+    const matchingCatsArray = catsData.filter(function (cats) {
+      if (isGifChecked) {
+        return cats.emotionTags.includes(selectedEmotion) && cats.isGif;
+      }
+
+      return cats.emotionTags.includes(selectedEmotion);
+    });
+
+    return matchingCatsArray;
+  }
+}
+
+function getSingleMatchingCat() {
+  const matchingCats = getMatchingCatsArray();
+  const randomNum = Math.floor(Math.random() * matchingCats.length);
+  console.log(randomNum);
+
+  if (matchingCats.length === 1) {
+    return matchingCats[0];
+  } else {
+    return matchingCats[randomNum];
+  }
 }
 
 function highlightRadio(event) {
